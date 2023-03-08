682178e2c07ce9e6eb7989d434f2b5d3b95fc3e5
isaaclee334 <125384108+isaaclee334@users.noreply.github.com>
2023-03-08 12:00:05 -0300

    Add files via upload

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -45,25 +45,25 @@ def drawBox(img,bbox):
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
     cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
-while True:
-    
-    check, img = video.read()   
 
-    # Atualize o rastreador em img e na caixa delimitadora
-    success, bbox = tracker.update(img)
+while True:
+    check,img = video.read()   
+    success,bbox = tracker.update(img)
 
     if success:
-        drawBox(img, bbox)
+        drawBox(img,bbox)
     else:
-        cv2.putText(img,"errou",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7(0,0,255),2)
+        cv2.putText(img,"Errou",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
 
     goal_track(img,bbox)
 
-    cv2.imshow("resultado", img)
+    cv2.imshow("resultado",img)
             
-    key = cv2.waitKey(25)
-    if key == 32:
-        print("Interrompido")
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print("Fechando")
         break
+
 video.release()
-cv2.destroyALLwindows()
+cv2.destroyALLwindows() 
